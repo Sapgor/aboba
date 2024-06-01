ebee831b388b01df4b85f55bf732f9aca6e4d76c
Sapgor <vasyuckov.n@yandex.ru>
2024-06-01 08:54:33 +0300

    aboba

---
+++ MashaPract8/Class1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace MashaPract8
+{
+    public static class SerializationHelper
+    {
+        public static T Deserialize<T>(string jsonData)
+        {
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+
+        public static string Serialize<T>(T obj)
+        {
+            return JsonSerializer.Serialize(obj);
+        }
+    }
+}
